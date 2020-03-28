e85a318a0cd31881e40f542d4019005f9c2fa33d
hmjubaer <hmjubaer50@gmail.com>
2020-03-28 13:09:31 +0600

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,44 @@
+// SCROLLIFY
+$(function() {
+  $.scrollify ({
+    section:'.scrollify',
+     setHeights: false,
+     updateHash: false
+  });
+});
+// HAMBURGER
+function closeMenu(){
+  document.getElementById('navbar').style.height="0%";
+}
+function openMenu(){
+  document.getElementById('navbar').style.height="100%";
+}
+// SMOOTH SCROLL
+$(function(){
+  $('.menu-item a').on('click', function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+          return false;
+  });
+});
+// STICKY MENU
+$(window).on('scroll', function(){
+  if($(window).scrollTop()){
+      $('nav').addClass('sticky');
+  }
+  else {
+        $('nav').removeClass('sticky');
+  }
+});
+// MIXITUP
+var mixer = mixitup('.grid-work-container');
+// COUNTER UP
+$('.num').counterUp({
+  time: 2000
+});
+// PRELODER
+$.LoadingOverlay("show");
+setTimeout(function(){
+    $.LoadingOverlay("hide");
+}, 1000);
